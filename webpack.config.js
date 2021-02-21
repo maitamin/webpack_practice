b1ba7e6f345282a0a70ef37bd6b25f3335bcb0a4
maitamin <3617510+maitamin@users.noreply.github.com>
2021-02-21 22:20:20 +0900

    キャッシュ対応のためファイル名にハッシュ値を設定。 ついでに不要ファイル削除。

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
     entry: './src/javascripts/main.js',
     output: {
         path: path.resolve(__dirname, './dist'),
-        filename: 'javascripts/main.js',
+        filename: 'javascripts/[name]-[contenthash].js',
         publicPath: '/',
     },
     module: {
@@ -70,7 +70,7 @@ module.exports = {
                 test: /\.(png|jpg|jpeg)/,
                 type: 'asset/resource',
                 generator: {
-                    filename: 'images/[name][ext]'
+                    filename: 'images/[name]-[contenthash][ext]'
                 }, 
                 use: [
                     {
@@ -108,7 +108,7 @@ module.exports = {
     plugins: [
         new VueLoaderPlugin(),
         new MiniCssExtractPlugin({
-            filename: './stylesheets/main.css',
+            filename: './stylesheets/[name]-[contenthash].css',
         }),
         new HtmlWebpackPlugin({
             template: './src/templates/index.pug',
